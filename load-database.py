777d1bbca8bf57e8b30a08aae0328608050c8c6d
Gaurav Sharma <gauravsharmags06@gmail.com>
2020-05-08 11:42:46 +0530

    database setup

--- load-database.py
+++ load-database.py
@@ -1,13 +1,18 @@
+import os
+import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','HotelManagement.settings')
+django.setup()
+
+
 from openpyxl import load_workbook
+from hotel.models import Hotel
+
 
 wb = load_workbook(filename='Dummy Listing.xlsx')
 sheet = wb['Sheet1']
 for row in range(2, 20):
-    vendor_name = sheet['{}{}'.format('A', row)].value
-    city = sheet['{}{}'.format('F', row)].value
-    venue = sheet['{}{}'.format('G', row)].value
-
-    
-    
-
-    
+    h = Hotel()
+    h.name = sheet['{}{}'.format('A', row)].value
+    h.city = sheet['{}{}'.format('F', row)].value
+    h.venue_type = sheet['{}{}'.format('G', row)].value
+    h.save()
