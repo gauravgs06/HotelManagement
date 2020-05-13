6d9eac1306d200532b3de7ec09f2cd1611e2b806
Gaurav Sharma <gauravsharmags06@gmail.com>
2020-05-13 09:41:36 +0530

    fixed extra page search

--- hotel/views.py
+++ hotel/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.http import Http404
 from .models import Vendor
 
 # Create your views here.
@@ -8,5 +9,7 @@ from .models import Vendor
 def listing(request, page):
     hotels = Vendor.objects.all()
     paginator = Paginator(hotels, 12)
+    if page > paginator.num_pages:
+        raise Http404("Page not found")
     pageObject = paginator.get_page(page)
     return render(request, 'hotels/listing.html', {"pageObjects": pageObject})
